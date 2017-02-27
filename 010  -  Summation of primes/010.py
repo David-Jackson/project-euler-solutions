9252d2c192572c2877553dd018ae22e0e65a1071
David Jackson <jack8679@kettering.edu>
2017-02-27 08:36:04 -0500

    Function name edit

--- 010  -  Summation of primes/010.py
+++ 010  -  Summation of primes/010.py
@@ -1,15 +1,15 @@
 # Sieve of Eratosthenes implementation
-def sumPrimeSieve(n):
+def sum_prime_sieve(n):
     l = [True] * (n + 1)
     for i in range(2, int(n**0.5) + 1):
         if l[i]:
             for j in range(i**2, n + 1, i):
                 l[j] = False
-        sum = 0
-        for i in range(2, n):
-            if l[i]:
-                sum += i
+    sum = 0
+    for i in range(2, n):
+        if l[i]:
+            sum += i
     return sum
 
 
-print sumPrimeSieve(int(2E6))
+print sum_prime_sieve(int(2E6))
